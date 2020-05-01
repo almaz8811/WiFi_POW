ef70eb3c6a3c79cbdf74743ba16737bb5334b3b3
AlMaz <almaz8811@gmail.com>
2020-05-01 13:50:01 +0300

    Добавил таблицу вывода значений на дисплей

--- src/Sensors.ino
+++ src/Sensors.ino
@@ -1,36 +1,43 @@
 // -----------------  DHT
-void DHT_init() {
-  dht.setup(dhtPin); //Запускаем датчик
-  delay(1000); // Нужно ждать иначе датчик не определится правильно
+void DHT_init()
+{
+  dht.setup(dhtPin);                                     //Запускаем датчик
+  delay(1000);                                           // Нужно ждать иначе датчик не определится правильно
   static uint16_t test = dht.getMinimumSamplingPeriod(); // Получим минимальное время между запросами данных с датчика
-  jsonWrite(configJson, "dhttime", test); // Отправим в json переменную configJson ключ dhttime полученное значение
-  dht.getTemperature();   // обязательно делаем пустое чтение первый раз иначе чтение статуса не сработает
-  String statusDHT = dht.getStatusString(); // Определим стстус датчика
+  jsonWrite(configJson, "dhttime", test);                // Отправим в json переменную configJson ключ dhttime полученное значение
+  dht.getTemperature();                                  // обязательно делаем пустое чтение первый раз иначе чтение статуса не сработает
+  String statusDHT = dht.getStatusString();              // Определим стстус датчика
   Serial.print("DHT = ");
   Serial.println(statusDHT); //  и сообщим в Serial
 
-  if (statusDHT == "OK") { // включим задачу если датчик есть
-    jsonWrite(configJson, "temperature", dht.getTemperature());  // отправить температуру в configJson 
-    jsonWrite(configJson, "humidity", dht.getHumidity());        // отправить влажность в configJson 
+  if (statusDHT == "OK")
+  {                                                             // включим задачу если датчик есть
+    jsonWrite(configJson, "temperature", dht.getTemperature()); // отправить температуру в configJson
+    jsonWrite(configJson, "humidity", dht.getHumidity());       // отправить влажность в configJson
 
-    ts.add(0, test, [&](void*) { // Запустим задачу 0 с интервалом test
-   jsonWrite(configJson, "temperature", dht.getTemperature());   // отправить температуру в configJson 
-   jsonWrite(configJson, "humidity", dht.getHumidity());         // отправить влажность в configJson 
-   Serial.print(".");
-    }, nullptr, true);
+    ts.add(
+        0, test, [&](void *) {                                        // Запустим задачу 0 с интервалом test
+          jsonWrite(configJson, "temperature", dht.getTemperature()); // отправить температуру в configJson
+          jsonWrite(configJson, "humidity", dht.getHumidity());       // отправить влажность в configJson
+          Serial.print(".");
+        },
+        nullptr, true);
   }
 }
 
 // -----------------  Вывод времени и даты в /config.live.json каждую секунду
-void sec_init() { 
-    ts.add(1, 1000, [&](void*) { // Запустим задачу 1 с интервалом 1000ms
-    // поместим данные для web страницы в json строку configJson
-    // Будем вызывать эту функцию каждый раз при запросе /config.live.json
-    // jsonWrite(строка, "ключ", значение_число); Так можно дабавить или обнавить json значение ключа в строке
-    // jsonWrite(строка, "ключ", "значение_текст");  
-    jsonWrite(configJson, "time", GetTime()); // отправить время в configJson 
-    jsonWrite(configJson, "date", GetDate()); // отправить дату в configJson
-    }, nullptr, true);  
+void sec_init()
+{
+  ts.add(
+      1, 1000, [&](void *) { // Запустим задачу 1 с интервалом 1000ms
+        // поместим данные для web страницы в json строку configJson
+        // Будем вызывать эту функцию каждый раз при запросе /config.live.json
+        // jsonWrite(строка, "ключ", значение_число); Так можно дабавить или обнавить json значение ключа в строке
+        // jsonWrite(строка, "ключ", "значение_текст");
+        jsonWrite(configJson, "time", GetTime()); // отправить время в configJson
+        jsonWrite(configJson, "date", GetDate()); // отправить дату в configJson
+      },
+      nullptr, true);
 }
 /* ---------------- Задание для закрепления материала
  *  Заставьте мигать светодиод на любом pin с частотой 5 секунд
@@ -40,18 +47,24 @@ void sec_init() {
  *  Процедуру blink_init() инициализируйте в setup
  */
 
-void disp_time() {
-    ts.add(2, 60000, [&](void*) { // Запустим задачу 1 с интервалом 1000ms
-    // поместим данные для web страницы в json строку configJson
-    // Будем вызывать эту функцию каждый раз при запросе /config.live.json
-    // jsonWrite(строка, "ключ", значение_число); Так можно дабавить или обнавить json значение ключа в строке
-    // jsonWrite(строка, "ключ", "значение_текст");  
-    display.startWrite();
-    display.setFont(&FreeMonoBold12pt7b);
-   display.setTextColor(ILI9341_GREEN);
-    display.fillRect(240, 0, 80, 20,   ILI9341_BLACK);
-      display.setCursor(240, 16);
-      display.print(GetTimeHM());
-      display.endWrite();
-    }, nullptr, true);  
-  }
+void disp_time()
+{
+  ts.add(
+      2, 60000, [&](void *) { // Запустим задачу 1 с интервалом 1000ms
+        // поместим данные для web страницы в json строку configJson
+        // Будем вызывать эту функцию каждый раз при запросе /config.live.json
+        // jsonWrite(строка, "ключ", значение_число); Так можно дабавить или обнавить json значение ключа в строке
+        // jsonWrite(строка, "ключ", "значение_текст");
+        display.startWrite();
+        display.setFont(&FreeMonoBold12pt7b);
+        display.setTextColor(ILI9341_GREEN);
+        display.fillRect(240, 0, 80, 20, ILI9341_BLACK);
+        display.setCursor(240, 16);
+        display.print(GetTimeHM());
+        display.endWrite();
+                  displayEE();
+      },
+      nullptr, true);
+}
+
+
--- src/main.ino
+++ src/main.ino
@@ -1,21 +1,24 @@
 // ------------- Чтение значения json
-String jsonRead(String &json, String name) {
+String jsonRead(String &json, String name)
+{
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject &root = jsonBuffer.parseObject(json);
   return root[name].as<String>();
 }
 
 // ------------- Чтение значения json
-int jsonReadtoInt(String &json, String name) {
+int jsonReadtoInt(String &json, String name)
+{
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject &root = jsonBuffer.parseObject(json);
   return root[name];
 }
 
 // ------------- Запись значения json String
-String jsonWrite(String &json, String name, String volume) {
+String jsonWrite(String &json, String name, String volume)
+{
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject &root = jsonBuffer.parseObject(json);
   root[name] = volume;
   json = "";
   root.printTo(json);
@@ -23,9 +26,10 @@ String jsonWrite(String &json, String name, String volume) {
 }
 
 // ------------- Запись значения json int
-String jsonWrite(String &json, String name, int volume) {
+String jsonWrite(String &json, String name, int volume)
+{
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject &root = jsonBuffer.parseObject(json);
   root[name] = volume;
   json = "";
   root.printTo(json);
@@ -33,27 +37,32 @@ String jsonWrite(String &json, String name, int volume) {
 }
 
 // ------------- Запись значения json float
-String jsonWrite(String &json, String name, float volume) {
+String jsonWrite(String &json, String name, float volume)
+{
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject &root = jsonBuffer.parseObject(json);
   root[name] = volume;
   json = "";
   root.printTo(json);
   return json;
 }
 
-void saveConfig (){
-  writeFile("config.json", configSetup );
+void saveConfig()
+{
+  writeFile("config.json", configSetup);
 }
 
 // ------------- Чтение файла в строку
-String readFile(String fileName, size_t len ) {
+String readFile(String fileName, size_t len)
+{
   File configFile = SPIFFS.open("/" + fileName, "r");
-  if (!configFile) {
+  if (!configFile)
+  {
     return "Failed";
   }
   size_t size = configFile.size();
-  if (size > len) {
+  if (size > len)
+  {
     configFile.close();
     return "Large";
   }
@@ -63,9 +72,11 @@ String readFile(String fileName, size_t len ) {
 }
 
 // ------------- Запись строки в файл
-String writeFile(String fileName, String strings ) {
+String writeFile(String fileName, String strings)
+{
   File configFile = SPIFFS.open("/" + fileName, "w");
-  if (!configFile) {
+  if (!configFile)
+  {
     return "Failed to open config file";
   }
   configFile.print(strings);
@@ -74,18 +85,18 @@ String writeFile(String fileName, String strings ) {
   return "Write sucsses";
 }
 
-
 // Перегрузка функций
 // ------------- Создание данных для графика
-String graf(int datas) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
+String graf(int datas)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
   // {"data":[1]}
   // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
   DynamicJsonBuffer jsonBuffer;
   // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
+  JsonObject &json = jsonBuffer.parseObject(root);
   // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
+  JsonArray &data = json.createNestedArray("data");
   data.add(datas);
   // Помещаем созданный json в переменную root
   root = "";
@@ -93,15 +104,16 @@ String graf(int datas) {
   return root;
 }
 // ------------- Создание данных для графика
-String graf(float datas) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
+String graf(float datas)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
   // {"data":[1]}
   // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
   DynamicJsonBuffer jsonBuffer;
   // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
+  JsonObject &json = jsonBuffer.parseObject(root);
   // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
+  JsonArray &data = json.createNestedArray("data");
   data.add(datas);
   // Помещаем созданный json в переменную root
   root = "";
@@ -109,34 +121,17 @@ String graf(float datas) {
   return root;
 }
 // ------------- Создание данных для графика
-String graf(int datas, int datas1) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
+String graf(int datas, int datas1)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
   // {"data":[1]}
   // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
   DynamicJsonBuffer jsonBuffer;
   // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
+  JsonObject &json = jsonBuffer.parseObject(root);
   // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
-  JsonArray& data1 = json.createNestedArray("data1");
-  data.add(datas);
-  data1.add(datas1);
-  // Помещаем созданный json в переменную root
-  root = "";
-  json.printTo(root);
-  return root;
-}
-// ------------- Создание данных для графика
-String graf(float datas, float datas1) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
-  // {"data":[1]}
-  // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
-  DynamicJsonBuffer jsonBuffer;
-  // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
-  // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
-  JsonArray& data1 = json.createNestedArray("data1");
+  JsonArray &data = json.createNestedArray("data");
+  JsonArray &data1 = json.createNestedArray("data1");
   data.add(datas);
   data1.add(datas1);
   // Помещаем созданный json в переменную root
@@ -145,37 +140,37 @@ String graf(float datas, float datas1) {
   return root;
 }
 // ------------- Создание данных для графика
-String graf(float datas, float datas1, float datas2) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
+String graf(float datas, float datas1)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
   // {"data":[1]}
   // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
   DynamicJsonBuffer jsonBuffer;
   // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
+  JsonObject &json = jsonBuffer.parseObject(root);
   // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
-  JsonArray& data1 = json.createNestedArray("data1");
-  JsonArray& data2 = json.createNestedArray("data2");
+  JsonArray &data = json.createNestedArray("data");
+  JsonArray &data1 = json.createNestedArray("data1");
   data.add(datas);
   data1.add(datas1);
-  data2.add(datas2);
   // Помещаем созданный json в переменную root
   root = "";
   json.printTo(root);
   return root;
 }
 // ------------- Создание данных для графика
-String graf(int datas, int datas1, int datas2) {
-  String root = "{}";  // Формировать строку для отправки в браузер json формат
+String graf(float datas, float datas1, float datas2)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
   // {"data":[1]}
   // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
   DynamicJsonBuffer jsonBuffer;
   // вызовите парсер JSON через экземпляр jsonBuffer
-  JsonObject& json = jsonBuffer.parseObject(root);
+  JsonObject &json = jsonBuffer.parseObject(root);
   // Заполняем поля json
-  JsonArray& data = json.createNestedArray("data");
-  JsonArray& data1 = json.createNestedArray("data1");
-  JsonArray& data2 = json.createNestedArray("data2");
+  JsonArray &data = json.createNestedArray("data");
+  JsonArray &data1 = json.createNestedArray("data1");
+  JsonArray &data2 = json.createNestedArray("data2");
   data.add(datas);
   data1.add(datas1);
   data2.add(datas2);
@@ -184,79 +179,96 @@ String graf(int datas, int datas1, int datas2) {
   json.printTo(root);
   return root;
 }
-
-void displayEE(){
-   display.startWrite();
-   display.fillRect(0, 50, 320, 40,   ILI9341_BLACK);
-   display.drawRect(0, 50, 159, 20,   ILI9341_WHITE);
-   display.drawRect(160, 50, 159, 20, ILI9341_WHITE);
-   display.drawRect(0, 70, 159, 20,   ILI9341_WHITE);
-   display.drawRect(160, 70, 159, 20, ILI9341_WHITE);
-  
-   display.setFont(&FreeMonoBold12pt7b);
-   display.setTextColor(ILI9341_GREEN);
-
-   sprintf(s,"%d.%02d V",(int)u1,((int)(u1*100))%100);
-   display.setCursor(2, 68);
-   display.print(s);
-   
-   sprintf(s,"%d.%02d A",(int)i1,((int)(i1*100))%100);
-   display.setCursor(162, 68);
-   display.print(s);
-    
-   sprintf(s, "%d.%02d W", (int)p1 ,((int)(p1*100))%100);
-   display.setCursor(2, 88);
-   display.print(s);
-       
-   sprintf(s,"%d.%02d Wh",(int)e1,((int)(e1*100))%100);
-   display.setCursor(162, 88);
-   display.print(s);
-
-// Отображение времени
-   if( t_correct != 0 ){
-//      tm    = t_cur + t_correct;
-      sprintf(s, "%02d:%02d", (int)( tm/3600 )%24, (int)( tm/60 )%60);
-      display.fillRect(240, 0, 80, 20,   ILI9341_BLACK);
-      display.setCursor(240, 16);
-      display.print(s);
-    
-   }
-   
-   display.endWrite();   
+// ------------- Создание данных для графика
+String graf(int datas, int datas1, int datas2)
+{
+  String root = "{}"; // Формировать строку для отправки в браузер json формат
+  // {"data":[1]}
+  // Резервируем память для json обекта буфер может рости по мере необходимти, предпочтительно для ESP8266
+  DynamicJsonBuffer jsonBuffer;
+  // вызовите парсер JSON через экземпляр jsonBuffer
+  JsonObject &json = jsonBuffer.parseObject(root);
+  // Заполняем поля json
+  JsonArray &data = json.createNestedArray("data");
+  JsonArray &data1 = json.createNestedArray("data1");
+  JsonArray &data2 = json.createNestedArray("data2");
+  data.add(datas);
+  data1.add(datas1);
+  data2.add(datas2);
+  // Помещаем созданный json в переменную root
+  root = "";
+  json.printTo(root);
+  return root;
 }
 
-void displayGRAPH(){
-// Стираем область графика
-   display.startWrite();
-   display.fillRect(0, 120, 320, 120,   ILI9341_WHITE);
+void displayEE()
+{
+  display.startWrite();
+  display.fillRect(0, 38, 320, 60, ILI9341_BLACK);
+  display.drawRect(0, 38, 159, 20, ILI9341_WHITE);
+  display.drawRect(160, 38, 159, 20, ILI9341_WHITE);
+  display.drawRect(0, 58, 159, 20, ILI9341_WHITE);
+  display.drawRect(160, 58, 159, 20, ILI9341_WHITE);
+  display.drawRect(0, 78, 159, 20, ILI9341_WHITE);
+  display.drawRect(160, 78, 159, 20, ILI9341_WHITE);
 
+  display.setFont(&FreeMonoBold12pt7b);
+  display.setTextColor(ILI9341_GREEN);
 
-// Рисуем сетку
-   for( int i=0; i<15; i++){
-      int x = 18 + i*20;
-      display.drawLine(x,138,x,238,ILI9341_BLACK);    
-   }
-   for( int i=0; i<5; i++){
-      int y = 138 + i*20;
-      display.drawLine(18,y,318,y,ILI9341_BLACK);    
-   }
-// Формируем график
-   int n = 0;
-   sprintf(s, "Pmax=%d.%02d Pmin=%d.%02d", (int)p_max ,((int)(p_max*100))%100, (int)p_min ,((int)(p_min*100))%100);
-// Пишем минимум максимум
-   display.setFont(&FreeMono9pt7b);                           // устанавливаем маленький шрифт
-   display.setTextColor(ILI9341_RED);
-   display.setCursor(30,135);
-   display.print(s);
-      
-   for( int i=0; i<m_page_count0; i++){
-      float yf = 1;
-      if( p_max > 0 )yf = m_page0[i]*100/(p_max);
-      int y = 238-(int)yf;
-      int x  = 19 + n*2;
-      n++;
-      display.drawLine(x,y,x,237,ILI9341_RED);
-   }
+  sprintf(s, "%d.%02d V", (int)u1, ((int)(u1 * 100)) % 100);
+  display.setCursor(2, 55);
+  display.print(s);
 
-   display.endWrite();
+  sprintf(s, "%d.%02d A", (int)i1, ((int)(i1 * 100)) % 100);
+  display.setCursor(162, 55);
+  display.print(s);
+
+  sprintf(s, "%d.%02d W", (int)p1, ((int)(p1 * 100)) % 100);
+  display.setCursor(2, 75);
+  display.print(s);
+
+  sprintf(s, "%d.%02d Wh", (int)e1, ((int)(e1 * 100)) % 100);
+  display.setCursor(162, 75);
+  display.print(s);
+  display.endWrite();
+}
+
+void displayGRAPH()
+{
+  // Стираем область графика
+  display.startWrite();
+  display.fillRect(0, 120, 320, 120, ILI9341_WHITE);
+
+  // Рисуем сетку
+  for (int i = 0; i < 15; i++)
+  {
+    int x = 18 + i * 20;
+    display.drawLine(x, 138, x, 238, ILI9341_BLACK);
+  }
+  for (int i = 0; i < 5; i++)
+  {
+    int y = 138 + i * 20;
+    display.drawLine(18, y, 318, y, ILI9341_BLACK);
+  }
+  // Формируем график
+  int n = 0;
+  sprintf(s, "Pmax=%d.%02d Pmin=%d.%02d", (int)p_max, ((int)(p_max * 100)) % 100, (int)p_min, ((int)(p_min * 100)) % 100);
+  // Пишем минимум максимум
+  display.setFont(&FreeMono9pt7b); // устанавливаем маленький шрифт
+  display.setTextColor(ILI9341_RED);
+  display.setCursor(30, 135);
+  display.print(s);
+
+  for (int i = 0; i < m_page_count0; i++)
+  {
+    float yf = 1;
+    if (p_max > 0)
+      yf = m_page0[i] * 100 / (p_max);
+    int y = 238 - (int)yf;
+    int x = 19 + n * 2;
+    n++;
+    display.drawLine(x, y, x, 237, ILI9341_RED);
+  }
+
+  display.endWrite();
 }
